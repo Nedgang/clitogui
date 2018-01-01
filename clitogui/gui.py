aae6ee72ec4b99784c4992e059c89d0d6d94427c
Nedgang <nedgang@mailoo.org>
2018-01-01 16:04:17 +0100

    PyQt implementation (to finish)

--- clitogui/gui.py
+++ clitogui/gui.py
@@ -10,107 +10,96 @@ File containing the GUI stuff:
 # IMPORT #
 ##########
 import os
-import tkinter as tk
 import sys
 
 import argparse
 
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
 #########
 # CLASS #
 #########
-class Interface(tk.Frame):
-    def __init__(self, parent, clitogui_actions, *args, **kwargs):
-        self.parent = parent
-        self.parent.title(os.path.basename(sys.argv[0]).split(".")[0])
+class Interface():
+    def __init__(self, clitogui_actions, *args, **kwargs):
+        # Interface data initialization
+        self.title = os.path.basename(sys.argv[0]).split(".")[0]
         self.actions  = tuple(clitogui_actions)
         self.out_args = [] # Will be populated with args
-        self.holders  = {} # value holder of widgets
+        self.results  = {} # Widgets final values
+        self.hash_table = {}
+        print(self.actions)
+        for x in self.actions:
+            print(type(x))
+            if x.option_strings == []:
+                self.hash_table[x.dest] = x.option_strings
+            else:
+                self.hash_table[x.dest] = x.option_strings[0]
+        # GUI configuration
+        # Application initialization
+        self.application = QApplication(sys.argv)
 
         # LAUNCH WIDGETS CREATION
-        self.__create_widgets()
+        self.__create_window__()
 
-    def __create_widgets(self):
+    def __create_window__(self):
         """
-        Creation of a widget for each action in the parser.
+        Creation of the window, and associated layout.
         """
-        for idx, action in enumerate(self.actions):
-            # Label each widget with the help text.
-            lab = tk.Label(self.parent, text=action.help, width=40)
-            lab.grid(row = idx, column = 0)
-
-            # If choices are proposed:
+        # Layouts definition
+        widget_layout = QFormLayout()
+        main_layout = QVBoxLayout()
+        # Interaction buttons
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok\
+                                   |QDialogButtonBox.Cancel)
+        # Buttons link to dialog box
+        dialog = QDialog()
+        buttons.accepted.connect(dialog.accept)
+        buttons.rejected.connect(QCoreApplication.instance().quit)
+        # Layouts relations
+        main_layout.addLayout(widget_layout)
+        main_layout.addWidget(buttons)
+        dialog.setLayout(main_layout)
+        # Creation of arguments widgets
+        for action in self.actions:
             if action.choices != None:
-                type_return = str
-                holder = tk.Variable(value = action.choices[0])
-                wid = tk.OptionMenu(self.parent, holder, *action.choices)
-
+                widget = QComboBox()
+                widget.addItems([str(i) for i in action.choices])
             else:
-                if type(action) == argparse._StoreAction:
-                    type_return = str
-                    holder = tk.Variable(value = action.default)
-                    wid = tk.Entry(self.parent, textvariable = holder)
-
-                elif type(action) == argparse._CountAction:
-                    type_return = int
-                    count_choice = range(0, 10)
-                    holder = tk.Variable(value = count_choice[0])
-                    wid = tk.OptionMenu(self.parent, holder, *count_choice)
-
-                elif type(action) == argparse._AppendAction:
-                    type_return = "append_action"
-                    holder = tk.Variable(value = action.default)
-                    wid = tk.Entry(self.parent, textvariable = holder)
-
+                if type(action) == argparse._StoreTrueAction\
+                        or type(action) == argparse._StoreFalseAction:
+                    widget = QCheckBox()
+                    widget.setCheckState(action.default)
                 elif type(action) == argparse._StoreConstAction:
-                    type_return = bool
-                    holder = tk.BooleanVar()
-                    wid = tk.Checkbutton(self.parent, variable = holder)
-
-                elif type(action) == argparse._StoreTrueAction\
-                    or type(action) == argparse._StoreFalseAction:
-                    type_return = bool
-                    holder = tk.BooleanVar(value = action.default)
-                    wid = tk.Checkbutton(self.parent, variable = holder)
-
+                    widget = QCheckBox()
+                elif type(action) == argparse._StoreAction:
+                    widget = QLineEdit(action.default)
+                elif type(action) == argparse._CountAction:
+                    widget = QComboBox()
+                    widget.addItems([str(i) for i in range[0,10]])
+                elif type(action) == argparse._AppendAction:
+                    widget = QLineEdit(action.default)
+                elif type(action) == argparse._AppendAction:
+                    widget = QLineEdit(action.default)
                 else:
                     raise TypeError("Unhandled type: {}".format(type(action)))
-
-            wid.grid(row=idx, column=1)
-
-            if action.option_strings == []:
-                self.holders[""] = (type_return, holder)
-            else:
-                self.holders[action.option_strings[0]] = (type_return, holder)
-
-        # Launch button
-        tk.Button(self.parent, text="Run!", command=self.run).\
-            grid(row=len(self.actions), column = 0, columnspan=2)
-
-    def run(self):
-        """
-        Return values from the GUI.
-        """
-        for option, (type_return, holder) in self.holders.items():
-            # Positionnal argument (buggy if more than 1)
-            if option == '':
-                self.out_args.append(str(holder.get()))
-            # Boolean arguments
-            elif type_return == bool:
-                if holder.get() == True:
-                    self.out_args.append(option)
-            # Count arguments
-            elif type_return == int:
-                for i in range(holder.get()):
-                    self.out_args.append(option)
-            # Append arguments
-            elif type_return == "append_action":
-                for answer in str(holder.get()).split():
-                    self.out_args.append(option)
-                    self.out_args.append(answer)
-            # Standard arguments
-            else:
-                self.out_args.append(option)
-                self.out_args.append(str(holder.get()))
-
-        # Kill the GUI
-        self.parent.quit()
+            widget.setToolTip(action.help)
+            widget_layout.addRow(action.dest, widget)
+        # Widgets values recuperation
+        if dialog.exec() == QDialog.Accepted:
+            for i in range(widget_layout.rowCount()):
+                # Find the widget at position i
+                widget = widget_layout.itemAt(i, QFormLayout.FieldRole).widget()
+                # Find widget label
+                label  = widget_layout.labelForField(widget).text()
+                # Find widget value
+                value  = widget.metaObject().userProperty().read(widget)
+                self.results[label] = value
+            print(self.results)
+        # Writing the CLI from widgets values
+        print(self.hash_table)
+        for dest in self.results:
+            if self.hash_table[dest] != []:
+                self.out_args.append(self.hash_table[dest])
+            self.out_args.append(self.results[dest])
+        print(self.out_args)
