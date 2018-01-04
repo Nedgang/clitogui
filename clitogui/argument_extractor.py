768aa42d6f49fcc7cdb1a497e890d1be3bf0c390
Nedgang <nedgang@mailoo.org>
2018-01-04 22:27:30 +0100

    Cleaner type and default

--- clitogui/argument_extractor.py
+++ clitogui/argument_extractor.py
@@ -24,17 +24,31 @@ class Extracted_parser():
     def _argparse_extractor_(self, parser):
         """
         """
+        # We don't want help actions
+        parser._actions = [x for x in parser._actions \
+                           if type(x) != argparse._HelpAction]
         self.subparsers = [action for action in parser._actions\
                            if type(action) == argparse._SubParsersAction]
+        # In case of no subparser:
         if self.subparsers == []:
             for action in parser._actions:
-                if type(action) == argparse._HelpAction:
-                    pass
+                arg = {}
+                arg['cli'] = action.option_strings
+                arg['name'] = action.dest
+                arg['choices'] = action.choices
+                arg['help'] = action.help
+                arg['default'] = action.default
+                if type(action) == argparse._StoreAction:
+                    arg['type'] = str
+                elif type(action) == argparse._StoreTrueAction or\
+                        type(action) == argparse._StoreConstAction or\
+                        type(action) == argparse._StoreFalseAction:
+                    arg['type'] = bool
+                elif type(action) == argparse._AppendAction:
+                    arg['type'] = "append_action"
+                elif type(action) == argparse._CountAction:
+                    arg['type'] = int
                 else:
-                    arg = {}
-                    arg["cli"] = action.option_strings
-                    arg["name"] = action.dest
-                    arg['choices'] = action.choices
-                    arg['help'] = action.help
-                    arg['type'] = type(action)
-                    self.arguments.append(arg)
+                    raise TypeError("Unsupported argument type: ", type(action))
+                self.arguments.append(arg)
+            print(self.arguments)
