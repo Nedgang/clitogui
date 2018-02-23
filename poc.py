2fe734e05b4ba900b0790b0e04fb0f80cf49f5a0
Nedgang <nedgang@mailoo.org>
2018-02-23 11:11:21 +0100

    Add a use case to poc

--- poc.py
+++ poc.py
@@ -42,6 +42,7 @@ def create_parser() -> argparse.ArgumentParser:
     # create the parser for the "a" command
     parser_a = subparsers.add_parser('a', help='a help')
     parser_a.add_argument('bar', type=int, help='bar help')
+    parser_a.add_argument('floz', type=int, help='bar help')
     # create the parser for the "b" command
     parser_b = subparsers.add_parser('b', help='b help')
     parser_b.add_argument('--baz', choices='XYZ', help='baz help')
